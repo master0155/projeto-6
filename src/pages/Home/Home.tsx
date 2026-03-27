5d72cf948487577e04fb04f89e44bbef3c403d24
Gabriel <gabrielsilvalima1994@gmail.com>
2026-03-27 13:08:01 -0300

    .

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,10 @@ import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { Container, Section, SectionTitle } from './style'
 import logo from '../../assets/images/logo.svg'
-import { useGetOnSaleQuery, useGetRestaurantesQuery } from '../../services/api'
+import { useGetOnSaleQuery } from '../../services/api'
 import { useMemo } from 'react'
 
 export const Home = () => {
-  const { data: restaurants = [], isLoading, isError } = useGetRestaurantesQuery()
   const {
     data: featuredRestaurants = [],
     isLoading: isLoadingFeatured,
@@ -20,21 +19,6 @@ export const Home = () => {
     )
     return Array.from(uniqueMap.values())
   }, [featuredRestaurants])
-
-  const featuredIds = useMemo(
-    () => new Set(uniqueFeaturedRestaurants.map((restaurant) => restaurant.id)),
-    [uniqueFeaturedRestaurants]
-  )
-
-  const uniqueRestaurants = useMemo(() => {
-    const uniqueMap = new Map(restaurants.map((restaurant) => [restaurant.id, restaurant]))
-    return Array.from(uniqueMap.values())
-  }, [restaurants])
-
-  const nonFeaturedRestaurants = useMemo(
-    () => uniqueRestaurants.filter((restaurant) => !featuredIds.has(restaurant.id)),
-    [featuredIds, uniqueRestaurants]
-  )
 
   return (
     <>
@@ -53,25 +37,6 @@ export const Home = () => {
           {uniqueFeaturedRestaurants.map((restaurant) => (
             <Card
               key={`featured-${restaurant.id}`}
-              food={restaurant.tipo}
-              image={restaurant.capa}
-              tag={restaurant.tipo}
-              note={restaurant.avaliacao.toString()}
-              title={restaurant.titulo}
-              description={restaurant.descricao}
-            />
-          ))}
-        </Container>
-      </Section>
-
-      <Section className="container">
-        <SectionTitle>Todos os restaurantes</SectionTitle>
-        <Container>
-          {isLoading && <p>Carregando restaurantes...</p>}
-          {isError && <p>Erro ao carregar restaurantes.</p>}
-          {nonFeaturedRestaurants.map((restaurant) => (
-            <Card
-              key={restaurant.id}
               food={restaurant.tipo}
               image={restaurant.capa}
               tag={restaurant.tipo}
